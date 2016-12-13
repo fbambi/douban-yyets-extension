63e2e191a1e42a3deb835e2611eb1f9c8892664d
LDZ <ldzwow@foxmail.com>
2016-12-13 23:31:33 +0800

    update

--- source/douban.js
+++ source/douban.js
@@ -1,9 +1,31 @@
-$(document).ready(function() {
+(function() {
+    'use strict';
 
     var movieExtension = new Vue({
-        el: 'movieExtension',
+        el: '#extensionWrap',
         data: {
-            name: 'etstWorld'
+
+            // head
+            name: 'EastWorld',
+            status:'第三季连载中',
+            lastModifiedTime:new Date().toDateString(),
+
+            // tab
+            seasons:[
+                '第一季',
+                '第二季',
+                '第三季'
+            ],
+
+            resolutions:[
+                'HR-HRTV',
+                'WEB-DL',
+                '720P',
+                '1080P'
+            ]
+
+
+
         }
 
 
@@ -11,5 +33,4 @@ $(document).ready(function() {
 
 
 
-
-});
+})();
