6a82c614e3323458e13047f8fe05144ca5437469
LDZ <ldzwow@foxmail.com>
2016-12-16 12:26:47 +0800

    view

--- source/douban.js
+++ source/douban.js
@@ -2,69 +2,150 @@
 
 
     var tvData;
+    var tvInfo;
 
-    $.ajax({
-        url: 'testData.json',
-        dataType: 'json',
-        success: function(data) {
-            tvData = data;
-            load();
-        },
+    // $.ajax({
+    //     url: 'testData.json',
+    //     dataType: 'json',
+    //     success: function(data) {
+    //         tvData = data;
+    //         load();
+    //     },
+    //
+    // });
 
-    });
+
+
+    tvData = window.testData;
+    tvInfo = window.testInfo;
+    load();
 
 
 
     function load() {
 
-        function FilmModel(data) {
+        function FilmModel(data, info) {
             this.data = data;
+            this.info = info;
         }
 
         FilmModel.prototype = {
 
             constructor: FilmModel,
 
+            getNameEn: function() {
+                return this.info.name_en;
+            },
+
+            getNameCn: function() {
+                return this.info.name_cn;
+            },
+
+            getUpdate: function() {
+                return this.info.update;
+            },
+
+            getStatus: function() {
+                return this.info.status.season + this.info.status.status;
+            },
+
             getSeasons: function() {
                 return Object.keys(this.data);
             },
 
-            getSeasonResolutions: function(whichSeason) {
-                return Object.keys(this.data[whichSeason]);
+            getSeasonResolutions: function(season) {
+                return Object.keys(this.data[season]);
+            },
+
+            getSeasonResolutionLinks: function(season, resolution) {
+                var arr = [];
+                var resolutionObj = this.data[season][resolution];
+
+                Object.keys(resolutionObj).forEach(function(el, index) {
+
+                    arr.push({
+                        name: el,
+                        link: resolutionObj[el]
+                    });
+
+
+                });
+
+                return arr;
             }
+
+
+
+
         };
 
 
-        var thisFilm = new FilmModel(tvData);
-        console.log(thisFilm);
-
-        var useData = {
-            seasons: thisFilm.getSeasons(),
-            resolutions: thisFilm.getSeasonResolutions('season_1')
-        };
-
-
-
-
-
-
+        var dataModel = new FilmModel(tvData, tvInfo);
 
         var movieExtension = new Vue({
             el: '#extensionWrap',
             data: {
 
+                isCur: true,
+
+                //active
+                curSeasonIndex: 0,
+                curResolutionIndex: 0,
+
+                curSeason: 'season_1',
+                curResolution: 'hr-hdtv',
+
                 // head
-                name: 'EastWorld',
-                status: '第三季连载中',
-                lastModifiedTime: new Date().toDateString(),
+                name_en: dataModel.getNameEn(),
+                name_cn: dataModel.getNameCn(),
+                status: dataModel.getUpdate(),
+                update: dataModel.getStatus(),
 
                 // tab
-                seasons: useData.seasons,
+                seasons: dataModel.getSeasons(),
+                resolutions: dataModel.getSeasonResolutions('season_1'),
 
-                resolutions: useData.resolutions
+                // linkes
+                links: dataModel.getSeasonResolutionLinks('season_1', 'hr-hdtv'),
 
 
 
+            },
+
+            methods: {
+
+                changeSelect: function(e, index) {
+
+                },
+                changeSeason: function(e, index) {
+                    this.curSeasonIndex = index;
+                    var season = e.target.getAttribute('data-type');
+                    if (season !== this.cueSeason) {
+                        this.changeLinks({
+                            season: season
+                        });
+                    }
+                },
+
+                changeResolution: function(e, index) {
+                    this.curResolutionIndex = index;
+                    var resolution = e.target.getAttribute('data-type');
+                    if (resolution !== this.resolution) {
+                        this.changeLinks({
+                            resolution: resolution
+                        });
+                    }
+                },
+
+                changeLinks: function(config) {
+                    var season = config.season || this.curSeason;
+                    var resolution = config.resolution || this.curResolution;
+                    var links = dataModel.getSeasonResolutionLinks(season, resolution);
+                    if (links) {
+                        this.links = links;
+                    }
+
+                }
             }
 
 
---
+++ source/testData.js
@@ -0,0 +1,104 @@
+window.testInfo = {
+    name_en: 'Marvels Agents of SHIELD',
+    name_cn: '神盾局特工',
+    update: new Date().toDateString(),
+    status: {
+        season: '第三季',
+        status: '连载中'
+    }
+};
+
+window.testData = {
+    "season_1": {
+        "hr-hdtv": {
+            "01": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "02": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "03": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "04": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "05": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "06": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "07": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "08": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "09": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv"
+        },
+        "web-dl": {
+            "01": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "02": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "03": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "04": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "05": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "06": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "07": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "08": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "09": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv"
+        },
+        "720p": {
+            "01": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "02": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "03": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "04": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "05": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "06": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "07": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "08": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "09": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv"
+        },
+        "1080p": {
+            "01": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "02": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "03": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "04": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "05": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "06": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "07": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "08": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "09": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv"
+        }
+    },
+    "season_2": {
+        "hr-hdtv": {
+            "01": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "02": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "03": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "04": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "05": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "06": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "07": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "08": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E02.中英字幕.BD-HR.AC3.1024X576.x264.mkv",
+            "09": "神盾局特工.Marvels.Agents.of.S.H.I.E.L.D.S01E01.中英字幕.BD-HR.AC3.1024X576.x264.mkv"
+        },
+        "web-dl": {
+            "01": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "02": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "03": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "04": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "05": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "06": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "07": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "08": "Marvel.Agents.Of.SHIELD.S01E02.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv",
+            "09": "Marvel.Agents.Of.SHIELD.S01E01.Pilot.720p.WEB-DL.DD5.1.H.264-ECI.mkv"
+        },
+        "720p": {
+            "01": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "02": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "03": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "04": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "05": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "06": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "07": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv",
+            "08": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.720p.HDTV.X264-DIMENSION.mkv",
+            "09": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.720p.HDTV.X264-DIMENSION.mkv"
+        },
+        "1080p": {
+            "01": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "02": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "03": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "04": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "05": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "06": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "07": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv",
+            "08": "Marvels.Agents.of.S.H.I.E.L.D.S01E02.1080p.HDTV.X264-DIMENSION.mkv",
+            "09": "Marvels.Agents.of.S.H.I.E.L.D.S01E01.1080p.HDTV.X264-DIMENSION.mkv"
+        }
+    }
+};
